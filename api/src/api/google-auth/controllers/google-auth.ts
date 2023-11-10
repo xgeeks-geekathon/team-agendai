f9e0293ca450cde371003546dc24e9225a310e14
Fábio Santos <fs@cvt.services>
2023-11-10 21:49:10 +0000

    feat: add googleapis

---
+++ api/src/api/google-auth/controllers/google-auth.ts
@@ -0,0 +1,29 @@
+import GoogleAuthService from '../services/google-auth';
+
+module.exports = {
+  /**
+   * Redirects users to the Google Authorization screen
+   */
+  async connect(ctx) {
+    ctx.redirect(GoogleAuthService.getGoogleAuthURL());
+  },
+
+  /**
+   * Callback endpoint for handling the OAuth2 callback and token exchange
+   */
+  async callback(ctx) {
+    const { code } = ctx.query;
+    try {
+      const data = await GoogleAuthService.getGoogleAccountFromCode(code);
+
+      // Handle sign-in, user creation, etc. with the returned data
+      // For example, you could sign in the user, attach data to their account, etc.
+
+      // Redirect user or send the data back to the frontend
+      ctx.body = data;
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = { message: 'Error while exchanging token.', error };
+    }
+  }
+};
