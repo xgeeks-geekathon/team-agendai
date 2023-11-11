631626ded1ed176931d32967bb697f49a27d8ec7
Fábio Santos <fs@cvt.services>
2023-11-11 15:09:31 +0000

    feat: add tasks module

--- front/src/modules/Attributes/client/attributesClient.formatter.ts
+++
@@ -1,6 +0,0 @@
-export const mapAttributeData = (data: Attributes.AttributeApi): Attributes.Attribute => ({
-  ...data.attributes,
-  id: data.id,
-  createdAt: new Date(data.attributes.createdAt),
-  updatedAt: new Date(data.attributes.updatedAt),
-});
--- front/src/modules/Attributes/config.ts
+++
@@ -1,4 +0,0 @@
-export const cacheKeys = {
-  getAttribute: 'getAttribute',
-  getAttributes: 'getAttributes',
-};
