14a3dfd56c74fb8660ca37a4fe4b690bf7b29936
Fábio Santos <fs@cvt.services>
2023-11-11 13:24:56 +0000

    feat: board mocks

--- front/src/modules/Boards/client/boardsClient.mocks.ts
+++ front/src/modules/Boards/client/boardsClient.mocks.ts
@@ -2,10 +2,8 @@ import { faker } from '@faker-js/faker';
 
 export const getFakeBoard = () => ({
   id: faker.string.uuid(),
-  title: faker.company.name(),
-  access_token: faker.string.uuid(),
-  created_at: faker.date.anytime().getUTCDate(),
-  updated_at: faker.date.anytime().getUTCDate(),
+  name: faker.company.name(),
+  accessToken: faker.string.uuid(),
 });
 
 export const getFakeBoards = () => {
