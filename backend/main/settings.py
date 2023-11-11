0951138db7155e94b3a7df686ad225a39f76252e
Antonio Lopes <crowing.sessions@gmail.com>
2023-11-11 18:17:00 +0000

    BE - fix middleware path

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'backend.main.decorators.AuthenticateUserWithDjangoSessionMiddleware',
+    "main.decorators.AuthenticateUserWithDjangoSessionMiddleware",
 ]
 
 ROOT_URLCONF = 'main.urls'
